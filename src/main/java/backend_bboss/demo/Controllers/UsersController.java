e4d695be34fde385f7540aa4e9f10b1d5b649bd8
chaimaehanabi <77066713+chaimaehanabi@users.noreply.github.com>
2021-05-18 17:34:27 +0200

    update JWT

--- src/main/java/backend_bboss/demo/Controllers/UsersController.java
+++ src/main/java/backend_bboss/demo/Controllers/UsersController.java
@@ -1,108 +1,24 @@
 package backend_bboss.demo.Controllers;
-import backend_bboss.demo.Domaine.JwtResponse;
-import backend_bboss.demo.Domaine.MessageResponse;
-import backend_bboss.demo.Models.ERole;
-import backend_bboss.demo.Models.Roles;
 import backend_bboss.demo.Models.Users;
-import backend_bboss.demo.Repository.RoleRepository;
 import backend_bboss.demo.Repository.UserRepository;
-import backend_bboss.demo.Request.LoginRequest;
-import backend_bboss.demo.Request.RegisterRequest;
-import backend_bboss.demo.Security.JWT.JwtUtils;
-import backend_bboss.demo.Services.UserDetailsImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.*;
-import java.util.stream.Collectors;
-
 //@Api(value=" users Management System", description="Operations pertaining to users in users Management System")
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-@RequestMapping("/api/BBBos")
+@RequestMapping("/api/authenticate")
 public class UsersController {
     @Autowired
-    AuthenticationManager authenticationManager;
-    @Autowired
-    UserRepository userRepository;
-    @Autowired
-    RoleRepository roleRepository;
-
-    @Autowired
-    PasswordEncoder encoder;
-
-    @Autowired
-    JwtUtils jwtUtils;
-    @Autowired
     UserRepository repository;
-    // http://localhost:4200/api/BBBos/login
-    @PostMapping("/login")
-    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
-        System.out.println("test");
-        System.out.println(loginRequest.getPassword());
-        System.out.println(loginRequest.getUsername());
-        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        String jwt = jwtUtils.generateJwtToken(authentication);
-        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
-        List<String> roles = userDetails.getAuthorities().stream().map(item -> item.getAuthority()).collect(Collectors.toList());
-        return ResponseEntity.ok(new JwtResponse(jwt,
-                userDetails.getId(),
-                userDetails.getUsername(),
-                roles));
-    }
-    // http://localhost:4200//register
-    @PostMapping("/register")
-    public ResponseEntity<?> registerUser( @RequestBody RegisterRequest signUpRequest) {
-        if (userRepository.existsByUsername(signUpRequest.getUsername())) {
-            return ResponseEntity
-                    .badRequest()
-                    .body(new MessageResponse("Username is already exist!")); }
-
-        //*	if (userRepository.existsByEmail(signUpRequest.getEmail())) {
-        //*return ResponseEntity
-        //*				.badRequest().body(new MessageResponse("Error: Email is already in use!"));}
-
-        // Create new user's account
-        Users user = new Users(signUpRequest.getUsername(), encoder.encode(signUpRequest.getPassword()));
-        Set<String> strRoles = signUpRequest.getRoles();
-        Set<Roles> roles = new HashSet<>();
-        if (strRoles == null) {
-            Roles userRole = roleRepository.findByName(ERole.ROLE_USER)
-                    .orElseThrow(() -> new RuntimeException("Role is not found."));
-            roles.add(userRole);
-        } else {
-            strRoles.forEach(role -> {
-                switch (role) {
-                    case "admin":
-                        Roles adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
-                                .orElseThrow(() -> new RuntimeException("Role is not found."));
-                        roles.add(adminRole);
-
-                        break;
-                    default:
-                        Roles userRole = roleRepository.findByName(ERole.ROLE_USER)
-                                .orElseThrow(() -> new RuntimeException("Role is not found."));
-                        roles.add(userRole);
-                }
-            });
-        }
-        user.setRoles(roles);
-        userRepository.save(user);
-        return ResponseEntity.ok(new MessageResponse("Registration successfully!"));
-    }
     //@ApiOperation(value = "View a list of available USRES", response = List.class)
 
     @GetMapping("/users/ALL")
--- src/main/java/backend_bboss/demo/Security/JWT/JwtUtils.java
+++ src/main/java/backend_bboss/demo/Security/JWT/JwtUtils.java
@@ -1,5 +1,6 @@
 package backend_bboss.demo.Security.JWT;
 import backend_bboss.demo.Models.Users;
+import backend_bboss.demo.Services.UserDetailsImpl;
 import io.jsonwebtoken.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,18 +12,25 @@ import java.util.Date;
 @Component
 public class JwtUtils {
 	private static final Logger logger = LoggerFactory.getLogger(JwtUtils.class);
-	@Value("loizenai.app.jwtSecret")
+	@Value("jwtLoizenai.comSecretKey")
 	private String jwtSecret;
-	@Value("loizenai.app.jwtExpiration")
+	@Value("86400")
 	private int jwtExpiration;
+
 	public String generateJwtToken(Authentication authentication) {
-		Users users = (Users) authentication.getPrincipal();
+
+		UserDetailsImpl userPrincipal = (UserDetailsImpl) authentication.getPrincipal();
+
 		return Jwts.builder()
-				.setSubject((users.getUsername()))
+				.setSubject((userPrincipal.getUsername()))
 				.setIssuedAt(new Date())
-				.setExpiration(new Date((new Date()).getTime() + jwtExpiration*1000))
+				.setExpiration(new Date((new Date()).getTime() + jwtExpiration * 1000))
 				.signWith(SignatureAlgorithm.HS512, jwtSecret)
 				.compact();
+	}
+
+	public String getUserNameFromJwtToken(String token) {
+		return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getSubject();
 	}
 
 	public boolean validateJwtToken(String authToken) {
@@ -30,25 +38,18 @@ public class JwtUtils {
 			Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
 			return true;
 		} catch (SignatureException e) {
-			logger.error("Invalid JWT signature -> Message: {} ", e);
+			logger.error("Invalid JWT signature: {}", e.getMessage());
 		} catch (MalformedJwtException e) {
-			logger.error("Invalid JWT token -> Message: {}", e);
+			logger.error("Invalid JWT token: {}", e.getMessage());
 		} catch (ExpiredJwtException e) {
-			logger.error("Expired JWT token -> Message: {}", e);
+			logger.error("JWT token is expired: {}", e.getMessage());
 		} catch (UnsupportedJwtException e) {
-			logger.error("Unsupported JWT token -> Message: {}", e);
+			logger.error("JWT token is unsupported: {}", e.getMessage());
 		} catch (IllegalArgumentException e) {
-			logger.error("JWT claims string is empty -> Message: {}", e);
+			logger.error("JWT claims string is empty: {}", e.getMessage());
 		}
 
 		return false;
-	}
-
-	public String getUserNameFromJwtToken(String token) {
-		return Jwts.parser()
-				.setSigningKey(jwtSecret)
-				.parseClaimsJws(token)
-				.getBody().getSubject();
 	}
 }
 
